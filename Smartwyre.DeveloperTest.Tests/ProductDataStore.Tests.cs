5a6851f6547c4fa2f34f9a994f6d63b70f929c3f
Lija Eapen <lija.eapen@just-eat.com>
2023-07-05 14:48:16 +0100

    Added unit test for Product Data Store and Rebate Data store

---
+++ Smartwyre.DeveloperTest.Tests/ProductDataStore.Tests.cs
@@ -0,0 +1,24 @@
+﻿
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
+using Xunit;
+
+namespace Smartwyre.DeveloperTest.Tests
+{
+    public  class ProductDataStoreTests
+    {
+        [Fact]
+        public void GetProduct_ShouldReturnProduct()
+        {
+            //Arrange
+            var productDataStore = new ProductDataStore();
+            var productIdentifier = "testproduct";
+
+            //Act
+            Product product = productDataStore.GetProduct(productIdentifier);
+
+            //Assert
+            Assert.NotNull(product);
+        }
+    }
+}
---
+++ Smartwyre.DeveloperTest.Tests/RebateDataStore.Tests.cs
@@ -0,0 +1,28 @@
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Smartwyre.DeveloperTest.Tests
+{
+    public class RebateDataStoreTests
+    {
+        [Fact]
+        public void GetRebate_ShouldReturnRebate()
+        {
+            //Arrange
+            var rebateDataStore = new RebateDataStore();
+            var rebateIdentifier = "testrebate";
+
+            //Act
+            Rebate rebate = rebateDataStore.GetRebate(rebateIdentifier);
+
+            //Assert
+            Assert.NotNull(rebate);
+        }
+    }
+}

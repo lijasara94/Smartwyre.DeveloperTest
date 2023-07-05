057dcec19fc829093be2cd924e165609bf8cb86a
Lija Eapen <lija.eapen@just-eat.com>
2023-07-05 15:44:14 +0100

    Updated the program.cs file to run the consoleapp

---
+++ Smartwyre.DeveloperTest.Runner/DIContainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+
+namespace Smartwyre.DeveloperTest.Runner
+{
+    public class DIContainer
+    {
+        public static ServiceProvider Configure()
+        {
+            var serviceCollection = new ServiceCollection().
+                                         AddTransient<IRebateDataStore, RebateDataStore>()
+                                        .AddTransient<IProductDataStore, ProductDataStore>()
+                                        .AddTransient<IRebateService<FixedRateRebateService>, FixedRateRebateService>()
+                                        .AddTransient<IRebateService<FixedCashRebateService>, FixedCashRebateService>()
+                                        .AddTransient<IRebateService<AmountPerUomRebateService>, AmountPerUomRebateService>()
+                                        ;
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using System;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
@@ -6,6 +10,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        var serviceProvider = DIContainer.Configure();
+        bool shouldContinue = true;
+        while (shouldContinue)
+        {
+            Console.WriteLine("To claculate rebate please enter \n" +
+                " \n" +
+                          "1. RebateIdentifier (string) \n");
+
+
+            var rebateIdentifier = Console.ReadLine();
+            Console.WriteLine("\n 2. ProductIdentifier (string)\n");
+            var productIdentifier = Console.ReadLine();
+
+            Console.WriteLine("\n 3. Volume (decimal)\n");
+            var volume = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Since database is not developed getting the incentive type as user input for now . \n" +
+                "Select the incentive type\n" +
+                " \n" +
+                          "1. FixedRateRebate \n" +
+                          "2. AmountPerUom \n" +
+                          "3. FixedCashAmount \n");
+
+            var option = Convert.ToInt32(Console.ReadLine());
+            CalculateRebateRequest calculateRebateRequest = new CalculateRebateRequest();   
+            calculateRebateRequest.RebateIdentifier = rebateIdentifier;
+            calculateRebateRequest.ProductIdentifier = productIdentifier;
+            calculateRebateRequest.Volume = volume;
+
+            var rebateDataStore = serviceProvider.GetService<IRebateDataStore>();
+            Rebate rebate = rebateDataStore.GetRebate(rebateIdentifier);
+
+            // gets the incentive tpe from user for now because the database is not developed yet
+            rebate.Incentive = (IncentiveType)(option-1);
+
+            if (rebate.Incentive == IncentiveType.FixedRateRebate)
+            {
+                var rebateservice = serviceProvider.GetService<IRebateService<FixedRateRebateService>>();
+                Console.WriteLine("CalculateRebateResult.Success = " + rebateservice.Calculate(calculateRebateRequest).Success + "\n");
+            }
+            else if (rebate.Incentive == IncentiveType.FixedCashAmount)
+            {
+                var rebateservice = serviceProvider.GetService<IRebateService<FixedCashRebateService>>();
+                Console.WriteLine("CalculateRebateResult.Success = " + rebateservice.Calculate(calculateRebateRequest).Success + "\n");
+            }
+            else if (rebate.Incentive == IncentiveType.AmountPerUom)
+            {
+                var rebateservice = serviceProvider.GetService<IRebateService<AmountPerUomRebateService>>();
+                Console.WriteLine("CalculateRebateResult.Success = " + rebateservice.Calculate(calculateRebateRequest).Success +"\n");
+            }
+
+            Console.WriteLine("Would you like to continue? Say 'Yes' Or 'No' \n");
+            var choice = Console.ReadLine();
+            if(choice.ToLower()=="yes")
+                shouldContinue = true;
+            else
+                shouldContinue = false;
+        }
     }
 }
